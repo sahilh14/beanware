9b297165f4ae6f8db14d71a8fd75296582cb398d
sahil gupta <sahil.gupta@iquanti.com>
2020-03-09 18:43:55 -0400

    Update wsgi.py

--- beanware/wsgi.py
+++ beanware/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gira.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beanware.settings")
 
 application = get_wsgi_application()
